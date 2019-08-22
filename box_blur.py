10366e70f1fa029fa1a1b0e3f6219f9f1af70e65
Varun Nair <nairv@purdue.edu>
2019-08-22 10:27:04 -0400

    box blur 2
    2nd update

--- box_blur.py
+++ box_blur.py
@@ -1,16 +1,9 @@
 import numpy as np
-<<<<<<< HEAD
 from scipy import signal
 import cv2
 import datetime
 
 n = 5
-=======
-import cv2
-import datetime
-
-n = 3
->>>>>>> master
 img = cv2.imread("/Users/varunvasudevan/Desktop/Purdue/5_Fin/Research/Wavelet Denoising/DSC6.JPG",0)
 dim = img.shape
 x = (dim[0]-(n-1))
@@ -26,7 +19,6 @@ for i in range(x):
         #img_copy[i][j] = (float(img[i][j])+float(img[i][j+1])+float(img[i][j+2])+float(img[i][j+3])+float(img[i][j+4])+float(img[i+1][j])+float(img[i+1][j+1])+float(img[i+1][j+2])+float(img[i+1][j+3])+float(img[i+1][j+4])+float(img[i+2][j])+float(img[i+2][j+1])+float(img[i+2][j+2])+float(img[i+2][j+3])+float(img[i+2][j+4])+float(img[i+3][j])+float(img[i+3][j+1])+float(img[i+3][j+2])+float(img[i+3][j+3])+float(img[i+3][j+3])+float(img[i+3][j+4])+float(img[i+4][j])+float(img[i+4][j+1])+float(img[i+4][j+2])+float(img[i+4][j+3])+float(img[i+4][j+4]))
         img_copy[i][j] = (img_copy[i][j])/(n**2)
 later = datetime.datetime.now()
-<<<<<<< HEAD
 print(later-now)
 print(img)
 print(img_copy)
@@ -38,15 +30,5 @@ cv2.imwrite("blurred.jpg", img_blur)
 #print(x*y*(n**2))
 print(later2-later)
 print(later2-now)
-=======
-print(img)
-print(img_copy)
-# print(im_dif)
-cv2.imwrite("orig.jpg", img)
-cv2.imwrite("blurred.jpg", img_copy)
-print(x*y*(n**2))
-print(c)
-print(later-now)
->>>>>>> master
 print("done")
 

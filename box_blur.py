33248ec497ce9162a6e98eb0c4b38bcf370eb63f
Varun Nair <nairv@purdue.edu>
2019-08-22 16:22:10 -0400

    box blur
    format update

--- box_blur.py
+++ box_blur.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import datetime
 
 n = 11
 img = cv2.imread("/Users/varunvasudevan/Desktop/Purdue/5_Fin/Research/Wavelet Denoising/DSC6.JPG",0)
@@ -9,7 +8,6 @@ x = (dim[0]-(n-1))
 y = (dim[1]-(n-1))
 dim_new=(x,y)
 img_copy = np.zeros(dim_new)
-now = datetime.datetime.now()
 for i in range(x):
     for j in range(y):
         for z in range (0, n):
@@ -23,8 +21,6 @@ for i in range(x):
         float(img[i+4][j+2])+float(img[i+4][j+3])+float(img[i+4][j+4]))
         """
         img_copy[i][j] = (img_copy[i][j])/(n**2)
-later = datetime.datetime.now()
-later2 = datetime.datetime.now()
 cv2.imwrite("orig.jpg", img)
 cv2.imwrite("blurred.jpg", img_copy)
 print("done")

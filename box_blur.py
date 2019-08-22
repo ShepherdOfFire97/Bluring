bff82be8f786c50226b5b3a4cb8243f8c1cb54cc
Varun Nair <nairv@purdue.edu>
2019-08-22 16:05:25 -0400

    gaussian blur using convolve
    Gaussian blur algorithm

--- box_blur.py
+++ box_blur.py
@@ -1,9 +1,8 @@
 import numpy as np
-from scipy import signal
 import cv2
 import datetime
 
-n = 5
+n = 11
 img = cv2.imread("/Users/varunvasudevan/Desktop/Purdue/5_Fin/Research/Wavelet Denoising/DSC6.JPG",0)
 dim = img.shape
 x = (dim[0]-(n-1))
@@ -16,19 +15,17 @@ for i in range(x):
         for z in range (0, n):
             for k in range (0, n):
                 img_copy[i][j] += float(img[i+z][j+k])
-        #img_copy[i][j] = (float(img[i][j])+float(img[i][j+1])+float(img[i][j+2])+float(img[i][j+3])+float(img[i][j+4])+float(img[i+1][j])+float(img[i+1][j+1])+float(img[i+1][j+2])+float(img[i+1][j+3])+float(img[i+1][j+4])+float(img[i+2][j])+float(img[i+2][j+1])+float(img[i+2][j+2])+float(img[i+2][j+3])+float(img[i+2][j+4])+float(img[i+3][j])+float(img[i+3][j+1])+float(img[i+3][j+2])+float(img[i+3][j+3])+float(img[i+3][j+3])+float(img[i+3][j+4])+float(img[i+4][j])+float(img[i+4][j+1])+float(img[i+4][j+2])+float(img[i+4][j+3])+float(img[i+4][j+4]))
+        """img_copy[i][j] = (float(img[i][j])+float(img[i][j+1])+float(img[i][j+2])+float(img[i][j+3])+
+        float(img[i][j+4])+float(img[i+1][j])+float(img[i+1][j+1])+float(img[i+1][j+2])+float(img[i+1][j+3])
+        +float(img[i+1][j+4])+float(img[i+2][j])+float(img[i+2][j+1])+float(img[i+2][j+2])+float(img[i+2][j+3])+
+        float(img[i+2][j+4])+float(img[i+3][j])+float(img[i+3][j+1])+float(img[i+3][j+2])+float(img[i+3][j+3])+
+        float(img[i+3][j+3])+float(img[i+3][j+4])+float(img[i+4][j])+float(img[i+4][j+1])+
+        float(img[i+4][j+2])+float(img[i+4][j+3])+float(img[i+4][j+4]))
+        """
         img_copy[i][j] = (img_copy[i][j])/(n**2)
 later = datetime.datetime.now()
-print(later-now)
-print(img)
-print(img_copy)
-# print(im_dif)
-img_blur = signal.convolve2d(img,img_copy,mode='same')
 later2 = datetime.datetime.now()
 cv2.imwrite("orig.jpg", img)
-cv2.imwrite("blurred.jpg", img_blur)
-#print(x*y*(n**2))
-print(later2-later)
-print(later2-now)
+cv2.imwrite("blurred.jpg", img_copy)
 print("done")
 

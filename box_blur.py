d22978bb4c6ed31822a4f0364ce69a66488f42cd
Varun Nair <nairv@purdue.edu>
2019-08-23 11:13:03 -0400

    box blur

--- box_blur.py
+++ box_blur.py
@@ -1,27 +1,30 @@
+"""This program uses for loops to create a blurred image using the box blur algorithm."""
+
 import numpy as np
 import cv2
 
-n = 11
-img = cv2.imread("",0)
-dim = img.shape
-x = (dim[0]-(n-1))
-y = (dim[1]-(n-1))
-dim_new=(x,y)
-img_copy = np.zeros(dim_new)
-for i in range(x):
-    for j in range(y):
-        for z in range (0, n):
-            for k in range (0, n):
-                img_copy[i][j] += float(img[i+z][j+k])  # following line is a hand written exp for a 5x5 kernel
-        """img_copy[i][j] = (float(img[i][j])+float(img[i][j+1])+float(img[i][j+2])+float(img[i][j+3])+
-        float(img[i][j+4])+float(img[i+1][j])+float(img[i+1][j+1])+float(img[i+1][j+2])+float(img[i+1][j+3])
-        +float(img[i+1][j+4])+float(img[i+2][j])+float(img[i+2][j+1])+float(img[i+2][j+2])+float(img[i+2][j+3])+
-        float(img[i+2][j+4])+float(img[i+3][j])+float(img[i+3][j+1])+float(img[i+3][j+2])+float(img[i+3][j+3])+
-        float(img[i+3][j+3])+float(img[i+3][j+4])+float(img[i+4][j])+float(img[i+4][j+1])+
-        float(img[i+4][j+2])+float(img[i+4][j+3])+float(img[i+4][j+4]))
-        """
-        img_copy[i][j] = (img_copy[i][j])/(n**2)
-cv2.imwrite("orig.jpg", img)
-cv2.imwrite("blurred.jpg", img_copy)
-print("done")
+def box_blur(img_path, n):
+    """
+    Method to blur image using box blur algorithm
+    :param img_path: The image path
+    :param n: The size of the kernel
+    :return: Blurred image
+    """
+    img = cv2.imread(img_path,0)
+    dim = img.shape
+    x = (dim[0]-(n-1))
+    y = (dim[1]-(n-1))
+    dim_new = (x, y)
+    img_copy = np.zeros(dim_new)
+    for i in range(10, x):
+        for j in range(10, y):
+            for z in range(0, n):
+                for k in range(0, n):
+                    img_copy[i][j] += float(img[i+z][j+k])
+            img_copy[i][j] = (img_copy[i][j])/(n**2)
+    print (img)
+    cv2.imwrite("box_blur.jpg", img_copy)
+    return (img_copy)
 
+if __name__ == "__main__":
+    box_blur("orig.jpg",3)
